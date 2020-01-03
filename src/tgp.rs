6a18609ac3ea57365ae9d5e93bc7d6ad60b261f5
Nikolai Maas <nikolai.maas@student.kit.edu>
2020-01-03 01:50:21 +0100

    rename state to data

--- src/tgp.rs
+++ src/tgp.rs
@@ -3,18 +3,19 @@ use std::marker::PhantomData;
 // ----- Basic Definitions -----
 
 // TODO: better lifetime?
-// TODO: context instead of state -> adding new effects possible
+// TODO: context instead of data -> adding new effects possible
+// TODO: better API just using Fn?
 
 /**
- * An effect changes the state of the game.
+ * An effect changes the data of the game.
  */
 pub trait Effect<M>: 'static {
-    fn apply(&self, state: &mut M);
+    fn apply(&self, data: &mut M);
 }
 
 impl<M, F: Fn(&mut M) + 'static> Effect<M> for F {
-    fn apply(&self, state: &mut M) {
-        self(state)
+    fn apply(&self, data: &mut M) {
+        self(data)
     }
 }
 
@@ -22,15 +23,15 @@ impl<M, F: Fn(&mut M) + 'static> Effect<M> for F {
  * A dispatcher maps a state to a subcomponent (module) of itself.
  */
 pub trait Dispatcher<T, M>: Copy + 'static {
-    fn dispatch<'a>(&self, state: &'a mut T) -> &'a mut M;
+    fn dispatch<'a>(&self, data: &'a mut T) -> &'a mut M;
 }
 
 impl<T, M, F: Copy + 'static> Dispatcher<T, M> for F
 where
     for<'a> F: Fn(&'a mut T) -> &'a mut M,
 {
-    fn dispatch<'a>(&self, state: &'a mut T) -> &'a mut M {
-        self(state)
+    fn dispatch<'a>(&self, data: &'a mut T) -> &'a mut M {
+        self(data)
     }
 }
 
@@ -43,13 +44,13 @@ pub fn build_dispatcher<T: 'static, M: 'static, F: Fn(&mut T) -> &mut M + Copy +
 #[macro_export]
 macro_rules! dispatch {
     ($type:ty) => {
-        build_dispatcher(|state: &mut $type| state)
+        build_dispatcher(|data: &mut $type| data)
     };
     ($type:ty, $first:ident $(. $path:ident)*) => {
-        build_dispatcher(|state: &mut $type| { &mut state.$first$(.$path)* })
+        build_dispatcher(|data: &mut $type| { &mut data.$first$(.$path)* })
     };
     ($type:ty, $path:tt) => {
-        build_dispatcher(|state: &mut $type| { &mut state.$path })
+        build_dispatcher(|data: &mut $type| { &mut data.$path })
     }
 }
 
@@ -67,8 +68,8 @@ pub struct EffectWrapper<T: 'static, M: 'static, E: Effect<M>, D: Dispatcher<T, 
 impl<T: 'static, M: 'static, E: Effect<M>, D: Dispatcher<T, M>> Effect<T>
     for EffectWrapper<T, M, E, D>
 {
-    fn apply(&self, state: &mut T) {
-        self.effect.apply(self.dispatcher.dispatch(state))
+    fn apply(&self, data: &mut T) {
+        self.effect.apply(self.dispatcher.dispatch(data))
     }
 }
 
@@ -89,6 +90,6 @@ impl<T: 'static, M: 'static, D: Dispatcher<T, M>> Remap<T, M, D, Box<dyn Effect<
     for Box<dyn Effect<M>>
 {
     fn remap(self, dispatcher: D) -> Box<dyn Effect<T>> {
-        Box::new(move |state: &mut T| self.apply(dispatcher.dispatch(state)))
+        Box::new(move |data: &mut T| self.apply(dispatcher.dispatch(data)))
     }
 }
